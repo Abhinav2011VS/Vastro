7b6d19bf996eef875a78db91b21691e59f15248e
Abhinav2011VS <152204613+Abhinav2011VS@users.noreply.github.com>
2024-05-25 22:11:41 +0530

    Update uninstall.js

--- uninstall.js
+++ uninstall.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 
 const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
 
-async function uninstall() {
+// Function to remove Vastro files from the global bin directory
+function removeFilesFromBinDirectory() {
+  const binDirectory = process.env.npm_config_prefix ? path.join(process.env.npm_config_prefix, 'bin') : '/usr/local/bin';
+  const vastroExecutablePath = path.join(binDirectory, 'vastro');
+
+  try {
+    fs.unlinkSync(vastroExecutablePath);
+  } catch (error) {
+    console.error('Error removing files from bin directory:', error);
+    process.exit(1);
+  }
+}
+
+// Function to run the uninstallation logic
+async function uninstallVastro() {
   const answers = await inquirer.prompt([
     {
       type: 'confirm',
@@ -12,11 +28,16 @@ async function uninstall() {
   ]);
 
   if (answers.confirm) {
+    console.log('Uninstalling Vastro...');
+    
+    // Remove Vastro executable from bin directory
+    removeFilesFromBinDirectory();
+
     console.log('Vastro has been uninstalled.');
-    // Add your uninstall logic here
   } else {
     console.log('Uninstallation cancelled.');
   }
 }
 
-uninstall();
+// Run the uninstallation logic
+uninstallVastro();
